da62506559e47e56572220ae4563b0161070b005
MarcSerraPeralta <marcserraperalta@gmail.com>
2023-12-29 18:10:27 +0100

    Add get_pij_matrix in init file

--- dem_estimation/__init__.py
+++ dem_estimation/__init__.py
@@ -1,6 +1,6 @@
 from . import dem_estimation
 from . import utils
 from .dem_estimation import get_edge_probabilities
-from .utils import stim_to_edges, edges_to_stim
+from .utils import stim_to_edges, edges_to_stim, get_pij_matrix
 
-__all__ = ["get_edge_probabilities", "stim_to_edges", "edges_to_stim"]
+__all__ = ["get_edge_probabilities", "stim_to_edges", "edges_to_stim", "get_pij_matrix"]
